5cebb0653c8d08f069b52244eaa18c11c725c066
Edward Cheng <eds_dev@outlook.com>
2017-12-15 20:55:15 +0800

    added framework

---
+++ SignalProcessingPlayground/Playground.fs
@@ -0,0 +1,88 @@
+﻿module Playground
+
+open System
+open MathNet.Numerics
+open MathNet.Filtering
+open MathNet.Numerics.IntegralTransforms
+
+type Signal = { Generator: float -> int -> float[]; Scalar: float; }
+
+// sample a signal *s* under sample rate *sr* and yield *n* points
+let sample sr n s =
+    s.Generator sr n |> Array.map (fun x -> x * s.Scalar)
+
+// sacle a signal *s* by a coeffcient *k*
+let scale k s =
+    { s with Scalar = k * s.Scalar }
+
+// zip two signal *s1* and *s2* with an operator *op*
+let zip op s1 s2 =
+    let generator = fun sr n -> Array.map2 op (sample sr n s1) (sample sr n s2)
+    { Generator = generator; Scalar = 1. }
+
+type Signal with
+    static member (~-) s = scale -1. s
+
+    static member (+) (lhs, rhs) = zip (+) lhs rhs
+    static member (-) (lhs, rhs) = zip (-) lhs rhs
+    static member (*) (lhs, rhs) = zip (*) lhs rhs
+    static member (/) (lhs, rhs) = zip (/) lhs rhs
+
+module Signal =
+    let arbitrarySignal func scalar =
+        let generator = fun sr n -> Array.init n (fun i -> (float i) / sr |> func)
+        { Generator = generator; Scalar = scalar; }
+
+    let zero scalar = arbitrarySignal (fun _ -> 0.) scalar
+    let step scalar = arbitrarySignal (fun _ -> 1.) scalar
+
+    let sine freq scalar =
+        let f = fun t -> sin (2.*Math.PI*freq*t)
+        arbitrarySignal f scalar
+    let cosine freq scalar =
+        let f = fun t -> cos (2.*Math.PI*freq*t)
+        arbitrarySignal f scalar
+
+    let whiteGaussian = 
+        let source = DataSources.WhiteGaussianNoiseSource()
+        arbitrarySignal (fun _ -> source.ReadNextSample()) 1.
+
+type Filter = Signal -> Signal
+module Filter =
+    let arbitraryFilter transform signal =
+        let generator = fun sr n -> transform sr <| sample sr n signal
+        { Generator = generator; Scalar = 1.; }
+
+    let lowPass cutoff = 
+        let createFilter sr = OnlineFilter.CreateLowpass(ImpulseResponse.Finite, sr, cutoff)
+        arbitraryFilter <| fun sr sample -> (createFilter sr).ProcessSamples(sample)
+
+    let highPass cutoff =
+        let createFilter sr = OnlineFilter.CreateHighpass(ImpulseResponse.Finite, sr, cutoff)
+        arbitraryFilter <| fun sr sample -> (createFilter sr).ProcessSamples(sample)
+    
+    let bandPass cutoffLow cutoffHigh =
+        let createFilter sr = OnlineFilter.CreateBandpass(ImpulseResponse.Finite, sr, cutoffLow, cutoffHigh)
+        arbitraryFilter <| fun sr sample -> (createFilter sr).ProcessSamples(sample)
+
+    let bandStop cutoffLow cutoffHigh =
+        let createFilter sr = OnlineFilter.CreateBandstop(ImpulseResponse.Finite, sr, cutoffLow, cutoffHigh)
+        arbitraryFilter <| fun sr sample -> (createFilter sr).ProcessSamples(sample)
+
+    let denoise =
+        let filter = OnlineFilter.CreateDenoise()
+        arbitraryFilter <| fun sr sample -> filter.ProcessSamples(sample)
+
+// calculate points of sampling time
+let samplePoints sr n =
+    Array.init n (fun i -> (float i) / sr)
+
+// calculate points of fft frequency
+let fftPoints sr n =
+    Fourier.FrequencyScale(n, sr)
+
+let fft x =
+   let x' = x |> Array.map (fun r -> complex r 0.)
+   Fourier.Forward(x')
+
+   x' |> Array.map Complex.magnitude
--- SignalProcessingPlayground/Program.fs
+++ SignalProcessingPlayground/Program.fs
@@ -1,7 +1,27 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Playground
+open XPlot.Plotly
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    let sr = 400.
+    let n = 400
+
+    let fb = Signal.sine 2. 1.
+    let fc = Signal.cosine 50. 3.
+    let s = fb * fc 
+    let x = s + scale 2. Signal.whiteGaussian
+    let y = x |> Filter.bandPass 43. 53.
+    let result = y * Signal.cosine 50. 1. |> Filter.lowPass 3.
+
+    sample sr n result
+    |> Seq.zip (samplePoints sr n)
+    |> Chart.Line
+    |> Chart.Show
+
+    sample sr n result
+    |> fft
+    |> Seq.zip (fftPoints sr n)
+    |> Chart.Line
+    |> Chart.Show
+
     0 // return an integer exit code
